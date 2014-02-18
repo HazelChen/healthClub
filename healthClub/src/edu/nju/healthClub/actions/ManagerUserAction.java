7807c76bb1ae1e44a0d8ecb9a081a993e014f8db
Hazel <cfzljcl@gmail.com>
2014-02-18 12:53:26 +0800

    修改user.html ing

---
+++ healthClub/src/edu/nju/healthClub/actions/ManagerUserAction.java
@@ -0,0 +1,20 @@
+package edu.nju.healthClub.actions;
+
+import java.util.ArrayList;
+
+public class ManagerUserAction extends BaseAction{
+	private ArrayList<String> months;
+	
+	@Override
+	public String execute () {
+		months = new ArrayList<>();
+		months.add("2014年1月");
+		months.add("2013年12月");
+		months.add("2013年11月");
+		months.add("2013年10月");
+		months.add("2013年9月");
+		return SUCCESS;
+	}
+	
+
+}
