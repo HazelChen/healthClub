e9d29e76ec215535ddfb7bc114fd92ce1fc79eaf
Hazel <cfzljcl@gmail.com>
2014-02-26 20:08:25 +0800

    加了一大堆lib

---
+++ healthClub/src/edu/nju/healthClub/model/Bank.java
@@ -0,0 +1,20 @@
+package edu.nju.healthClub.model;
+
+public class Bank {
+	private String id;
+	private int balance;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
+}
